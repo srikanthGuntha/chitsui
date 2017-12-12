98d10e73bc98bfacad3f6ecb77369fd595b899a0
Venkatesh Adigicherla <venkatesh.adigicherla@spglobal.com>
2017-12-12 22:51:26 +0530

    Added Groups component

---
+++ src/app/groups/groups.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-groups',
+  templateUrl: './groups.component.html',
+  styleUrls: ['./groups.component.scss']
+})
+export class GroupsComponent implements OnInit {
+	private chitData:any[];
+
+  constructor() { }
+
+  ngOnInit() {
+  	this.chitData = [
+  	{"value": 100000, "months": 50, "subscription": 2000, "number": "SSC002"},
+  	{"value": 100000, "months": 50, "subscription": 2000, "number": "SSC002"},
+  	{"value": 100000, "months": 50, "subscription": 2000, "number": "SSC002"},
+  	{"value": 100000, "months": 50, "subscription": 2000, "number": "SSC002"},
+  	{"value": 100000, "months": 50, "subscription": 2000, "number": "SSC002"},
+  	{"value": 100000, "months": 50, "subscription": 2000, "number": "SSC002"}
+  	];
+  }
+
+}
