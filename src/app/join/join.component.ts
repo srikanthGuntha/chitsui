a09ddd06e949a6532fad768ca6483d2bd1006f74
Venkatesh Adigicherla <venkatesh.adigicherla@spglobal.com>
2017-12-18 23:00:05 +0530

    Added Join Component submit functionality

--- src/app/join/join.component.ts
+++ src/app/join/join.component.ts
@@ -1,40 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../_services/authentication.service';
 
+import { FormBuilder,FormGroup, FormsModule,FormControl,  Validators , ReactiveFormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+
 @Component({
   selector: 'app-join',
   templateUrl: './join.component.html',
   styleUrls: ['./join.component.scss']
 })
 export class JoinComponent implements OnInit {
-	private userdata: any;
+  registrationForm : FormGroup;
+  
+  public user:any = {};
+  constructor(private formBuilder: FormBuilder, private authenticationService: AuthenticationService) {
+  }
 
-  	constructor(private authenticationService: AuthenticationService) { }
+  ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      name: [null, Validators.required],
+      dob: [null, Validators.required],
+      mobileNumber: [null, Validators.required],
+      lname: [null, Validators.required],
+      email: [null, [Validators.required]],
+      address: [null, Validators.required],
+      idNumber: [null, Validators.required],
+      IdType: [null, Validators.required]
+    });
+  }
 
-  	ngOnInit() {
-  	}
 
-  	public btnClickRegister():void {
-  		this.userdata = {
-  			firstname: "Rakesh",
-  			lastname: "Nagula",
-  			dateofbirth: new Date("10/12/1993"),
-  			email: "rockingrakesh10@gmail.com",
-  			mobile: 7366332243,
-  			password: "test123",
-  			addressline1: "1-98 peddha veedhi",
-  			addressline2: "jntu",
-  			district: "rangareddy",
-  			state: "Telangana",
-  			idtype: "Aadhar Card",
-  			idnumber: 119911991199
-  		};
+  isFieldValid(field: string) {
+    return !this.registrationForm.get(field).valid && this.registrationForm.get(field).touched;
+  }
 
-  		this.authenticationService.register(this.userdata)
-            .subscribe(result => {
-               console.log(result);
+  displayFieldCss(field: string) {
+    return {
+      'has-error': this.isFieldValid(field),
+      'has-feedback': this.isFieldValid(field)
+    };
+  }
+
+  submitForm() {
+    console.log(this.registrationForm);
+    if (this.registrationForm.valid) {
+      console.log('form submitted');
+      console.log(this.user);
+      this.authenticationService.register(this.user)
+             .subscribe(result => {
+                console.log(result);
             });
+    } else {
+      this.validateAllFormFields(this.registrationForm);
+      console.log(this.user);
+    }
+  }
 
-  	}
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      console.log(field);
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
 
 }
