4c7d6d60d38ef09c3f4c5bfdc313f5dd9f22b6ba
Pavan Kumar Bakku <bakkupavan@gmail.com>
2017-12-20 15:59:15 +0530

    added dummy tables in admin views

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,13 +9,16 @@ import { CommonComponent } from '../config/common.component';
 import { AdminComponent } from './admin.component';
 import { AdminHeaderComponent } from './header/header.component';
 import { AdminChitsComponent } from './chits/chits.component';
+import { AdminChitidsComponent } from './chitids/chitids.component';
 import { AdminBranchesComponent } from './branches/branches.component';
 
 const appAdminRoutes: Routes = [
-  { path: '', component: AdminChitsComponent },
+  { path: '', redirectTo: 'chits' },
   { path: '', component: AdminHeaderComponent, outlet:'header' },
-  {path: 'chits',component: AdminChitsComponent},
-  {path: 'branches',component: AdminBranchesComponent}
+  { path: 'chits', component: AdminChitsComponent},
+  { path: 'branches',component: AdminBranchesComponent},
+  { path: 'chitids', component: AdminChitidsComponent},
+  { path: 'admin/**', redirectTo: 'chits', pathMatch: 'full' }
 ];
 
 @NgModule({
@@ -24,7 +27,8 @@ const appAdminRoutes: Routes = [
     AdminComponent,
     AdminHeaderComponent,
 	  AdminChitsComponent,
-	  AdminBranchesComponent
+	  AdminBranchesComponent,
+    AdminChitidsComponent
   ],
   imports: [
     FormsModule,
