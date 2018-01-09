1748fdd59e219f166e2baf87e23833dcd5d1d41c
shiva shankar audam <shiva.shankar.audam@macys.com>
2018-01-09 18:05:06 +0530

    SA_admin : added chits. chitids and branches components for admin operations: SA

---
+++ src/app/admin/_services/chitid.services.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { globals } from '../../config/globals';
+import { CommonComponent } from '../../config/common.component';
+import { Observable } from 'rxjs';
+import {Branch} from '../../models/branch.model';
+import {ChitId} from '../../models/chitid.model';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ChitIdService{
+  public serviceUrl: string = globals.dbhosturl + "api/v1/";
+  public headers: any;
+  public commonComponent: CommonComponent;
+
+  constructor(private http: Http) {
+    this.commonComponent = new CommonComponent();
+    this.headers = this.commonComponent.getRequestHeaders();
+  }
+
+   getchitids(): Observable<any> {
+      return this.http.get(this.serviceUrl+ "getpopulate", {headers: this.headers})
+      .map((response: Response) => {
+        console.log(response);
+         response = response.json();
+         return response["data"];
+      });
+   }
+
+   addChitId(chitId : ChitId) : Observable < Response > {
+      return this
+            .http
+            .post(this.serviceUrl+'savechitids', {
+              chitid:chitId.chitid,
+              branch : chitId.branch
+            }, {headers:this.headers})
+   }
+
+   updateChitIds(id:string,chitId : ChitId): Observable<any> {
+      return this.http.put(this.serviceUrl+"updatechitids?id="+ chitId._id, {
+        branch:chitId.branch,
+        chitid:chitId.chitid
+      }, {headers: this.headers})
+         .map((response: Response) => {
+             console.log(response);
+         });
+   }
+
+   deleteChitIds(data): Observable<any> {
+      return this.http.delete(this.serviceUrl+"deletechitids?id="+data._id, {headers: this.headers})
+      .map((response: Response) => {
+          console.log(response);
+      });
+   }
+
+}
--- src/app/admin/chitids/chitids.component.ts
+++ src/app/admin/chitids/chitids.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ChitIdService } from '../_services/chitid.services';
+import { BranchService } from '../_services/branches.service';
+import {TemplateRef, ViewChild} from '@angular/core';
+import {Headers, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {ChitId} from '../../models/chitid.model';
 
 @Component({
   selector: 'app-admin-chitids',
@@ -7,9 +13,105 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminChitidsComponent implements OnInit {
 
-  constructor() { }
+	@ViewChild('readOnlyTemplate')readOnlyTemplate : TemplateRef < any >;
+    @ViewChild('editTemplate')editTemplate : TemplateRef < any >;
+
+    chitids :any;
+    selectedChitId : ChitId;
+    isNewRecord : boolean;
+    chitid:ChitId;
+    branches:any;
+
+    constructor(private chitIdService: ChitIdService, private branchService: BranchService) {
+
+     this.chitids = new Array < ChitId > ();
+  }
 
   ngOnInit() {
+       this.loadChitIds();
+       this.loadBranches();
+  }
+
+private loadBranches(){
+    this.branchService.getbranches()
+      .subscribe((resp : Response) => {
+        console.log(resp);
+        this.branches = resp;
+        //this.selectedBranch = this.branches[1];
+
+    });
+  }  
+
+  loadTemplate(chitid : ChitId) {
+     console.log(this.selectedChitId);
+        if (this.selectedChitId && this.selectedChitId._id == chitid._id) {
+            return this.editTemplate;
+        } else {
+            return this.readOnlyTemplate;
+        }
+         
+    }
+
+    addChitId(){
+       this.selectedChitId =new ChitId('','',''); 
+        this.chitids
+            .push(this.selectedChitId);
+        this.isNewRecord = true;
+
+    }
+
+    branchSelected(){
+        return this.selectedChitId;
+    }
+
+    cancel() {
+        this.chitids.pop();
+        this.selectedChitId = null;
+    }
+
+    editChitId(chitid : ChitId) {
+        this.selectedChitId = chitid;
+
+        console.log(this.selectedChitId);
+    }
+
+    saveChitId() {
+        if(this.isNewRecord){
+            //add a new Branch
+             this.chitIdService.addChitId(this.selectedChitId).subscribe((resp : Response) => {
+                this.chitid = resp.json(),
+                this.loadChitIds();
+                this.loadBranches();
+            });
+            this.isNewRecord=false;
+            this.selectedChitId = null;
+        }
+        else{
+            //edit the record
+             this.chitIdService.updateChitIds(this.selectedChitId._id,this.selectedChitId).subscribe((resp : Response) => {
+                this.loadChitIds();
+            });
+            this.selectedChitId = null;
+            
+        }
+    }
+
+    deleteChitIds(chitid : ChitId) {
+       this.chitIdService.deleteChitIds(chitid)
+         .subscribe(result => {
+           console.log(result);
+           this.loadChitIds();
+         });
+     }
+
+     
+
+  private loadChitIds(){
+     this.chitIdService.getchitids()
+      .subscribe((resp : Response) => {
+        console.log(resp);
+        this.chitids = resp;
+    });
   }
 
 }
