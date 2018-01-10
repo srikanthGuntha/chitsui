2e5f56b98344e7bb18e28dcd52cca01ae8939553
Venkatesh Adigicherla <venkatesh.adigicherla@spglobal.com>
2018-01-10 23:17:03 +0530

    Refactored My transactions page in user.

--- src/app/user/transaction/transaction.component.ts
+++ src/app/user/transaction/transaction.component.ts
@@ -12,6 +12,7 @@ export class TransactionComponent implements OnInit {
 
   public chitsData: any = [];
   public selectedChitId: string = "";
+  public showNoTransactionsText: string = "";
   public selectedChitsData: any = [];
   constructor(private authService: AuthenticationService,private chitsdataservice: ChitsService, private loaderService: LoaderService) { }
   ngOnInit() {
@@ -25,9 +26,13 @@ export class TransactionComponent implements OnInit {
               that.chitsData.push(data);
             }
           });
+          if (!this.chitsData.length) {
+            this.showNoTransactionsText = "All Your Chits are in Progress.";
+          }
         } else {
           this.loaderService.display(false);
           this.chitsData = [];
+          this.showNoTransactionsText = "Error in fetching your chits data.";
         }
       });
   }
